b1ce793fdc34764785042c2db22e4a6bb94c3d30
Ekaterina Nesterova <katea.salii@mail.ru>
2024-08-11 12:27:40 +0300

    Изменена основная логика Добавлена коробка

--- src/main/java/com/stepup/proj/MainApplication.java
+++ src/main/java/com/stepup/proj/MainApplication.java
@@ -3,6 +3,6 @@ package com.stepup.proj;
 public class MainApplication {
     public static void main(String[] args){
         System.out.println("Hello World!");
-        System.out.println("11111111");
+        System.out.println("1119991111");
     }
 }
