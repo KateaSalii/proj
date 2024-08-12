990194359721b6694f8a43fd2d1a49d2b297f119
Ekaterina Nesterova <katea.salii@mail.ru>
2024-08-12 10:50:34 +0300

    Добавлен вызов метода doSomething ()

--- src/main/java/com/stepup/proj/MainApplication.java
+++ src/main/java/com/stepup/proj/MainApplication.java
@@ -5,6 +5,7 @@ public class MainApplication {
         System.out.println("--------");
         System.out.println("Hello World!");
         System.out.println("1119991111");
+        doSomething();
         System.out.println("----");
     }
 
